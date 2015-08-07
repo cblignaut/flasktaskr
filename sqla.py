faba7c6b89a89fe311f6510cf69b215006f55cf8
SillyB <silly.blignaut@gmail.com>
2015-08-07 20:44:46 +0200

    Created Simple Blog

--- sqla.py
+++ sqla.py
@@ -5,8 +5,9 @@ conn = sqlite3.connect("new.db")
 
 cursor = conn.cursor()
 
-cursor.execute("""CREATE TABLE population
-                (city TEXT, state TEXT, population INT)
-                """)
+cursor.execute("INSERT INTO population VALUES('New York City', 'NY', 8200000)")
+cursor.execute("INSERT INTO population VALUES('San Francisco', 'CA', 800000)")
+
+conn.commit()
 
 conn.close()
