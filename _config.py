4b3fe9454ef5e4c248bd313a6ff3004495734f53
SillyB <silly.blignaut@gmail.com>
2015-08-14 22:16:25 +0200

    Flasktaskr changed to sqlalchemy integration

--- _config.py
+++ _config.py
@@ -4,7 +4,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 DATABASE = 'flasktaskr.db'
 DATABASE_PATH = os.path.join(basedir, DATABASE)
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_PATH
 USERNAME = 'admin'
 PASSWORD = 'admin'
-WTF_CSRF_ENABLED = True
-SECRET_KEY = """\xc2\xec\xc0L\x0b\x02e;<MB\xf7:)\xea\xd4\xab\xb5\xe1\x82K\xa6_\x17"""
+CSRF_ENABLED = True
+SECRET_KEY = """\xc2\xec\xc0L\x0b\x02e;<MB\xf7:)\xea\xd4\xab\xb5\xe1\x82K\xa6_\x17"""  
